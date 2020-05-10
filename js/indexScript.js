85eda7ba453602743440882fba4dfc94d7bf852c
Qian Zhuang <qianz@kean.edu>
2020-05-10 17:22:56 -0400

    click

--- js/indexScript.js
+++ js/indexScript.js
@@ -4,3 +4,11 @@ function sendVal(val) {
     var myUrl="./application.html"+"?"+"val="+val; 
     window.location.assign(myUrl);
  }
+
+ function goApp(){
+    window.location.assign("./application.html");
+ }
+
+ function goRoute(){
+    window.location.assign("./findRoute.html");
+ }
